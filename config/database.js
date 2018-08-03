0f4acc059fa07b4f60a39b8b1e64e54f4fb1a1d6
Bruno MC <brunomc@outlook.com>
2018-08-03 01:43:26 -0300

    s

--- config/database.js
+++ config/database.js
@@ -70,11 +70,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get('DB_HOST', 'tantor.db.elephantsql.com'),
+      port: Env.get('DB_PORT', '5432'),
+      user: Env.get('DB_USER', 'sajzbznp'),
+      password: Env.get('DB_PASSWORD', 'NtKyFuZYxpP3GSOgrTyuBDniTqpchn1l'),
+      database: Env.get('DB_DATABASE', 'sajzbznp')
     },
     debug: Env.get('DB_DEBUG', false)
   }
