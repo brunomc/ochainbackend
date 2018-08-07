04b389373e11556e884f107b3f225bb57e4d741f
Bruno MC <brunomc@outlook.com>
2018-08-07 01:04:13 -0300

    conexao com blockchain rinkeby

--- app/config/EthereumSetup.js
+++ app/config/EthereumSetup.js
@@ -1,7 +1,7 @@
 var Web3 = require('web3');
 //const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/BUYxMOgW9MGsKcg8rkeq"));
 const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/BUYxMOgW9MGsKcg8rkeq"));
-
+ 
 let TrackerProductABI = 
 [[
   {
